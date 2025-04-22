71a87d75767282affd0e5452f1d68a813a6c3ad9
MikeshETS <mikesh.chevli@kombee.com>
2025-04-22 10:13:43 +0530

    Integrate Partytown snippet into RootLayout for improved performance and tracking, ensuring proper initialization and logging.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import "@/app/globals.css";
 import GoogleTagManager from "@/components/GoogleTagManager";
+import { partytownSnippet } from "@builder.io/partytown/integration";
+import { partytownConfig } from "../partytown";
 
 export default function RootLayout({
   children,
@@ -10,6 +12,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <GoogleTagManager />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              ${partytownSnippet(partytownConfig)}
+              console.log('Partytown initialized:', window.partytown);
+            `,
+          }}
+        />
       </head>
       <body>{children}</body>
     </html>
