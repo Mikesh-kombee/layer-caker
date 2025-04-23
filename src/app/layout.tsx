92d7468f3d9f85996986e1314f4634fd698da3fa
MikeshETS <mikesh.chevli@kombee.com>
2025-04-23 11:23:25 +0530

    Integrate Google Tag Manager and Partytown component into RootLayout for improved tracking and performance optimization.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "@/app/globals.css";
 import Script from "next/script";
 import { partytownSnippet } from '@qwik.dev/partytown/integration';
 import { partytownConfig } from "../partytown";
+import GoogleTagManager from "@/components/GoogleTagManager";
+import { Partytown } from "@qwik.dev/partytown/react";
 
 export default function RootLayout({
   children,
@@ -12,6 +14,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <Partytown forward={["dataLayer.push"]} />
+        <GoogleTagManager />
         <Script
           id="partytown"
           strategy="beforeInteractive"
